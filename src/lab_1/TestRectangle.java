324ac5aeff248a7964f6c19f479d01008e9e14ce
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-10-16 23:59:51 +0700

    add TestRectangle Class/question1

---
+++ src/lab_1/TestRectangle.java
@@ -0,0 +1,22 @@
+//class TestRectangle with a main method to instantiate 5 different rectangles and visualize them.
+public class TestRectangle {
+    public static void main(String[] args) {
+        //instantiate 5 different rectangles
+        Rectangle r1 = new Rectangle(10, 5);
+        Rectangle r2 = new Rectangle(20, 10);
+        Rectangle r3 = new Rectangle(40, 20);
+        Rectangle r4 = new Rectangle(60, 40);
+        Rectangle r5 = new Rectangle(-20, -10);
+
+        //visualize them
+        r1.visualize();
+        System.out.println();
+        r2.visualize();
+        System.out.println();
+        r3.visualize();
+        System.out.println();
+        r4.visualize();
+        System.out.println();
+        r5.visualize();
+    }
+}
