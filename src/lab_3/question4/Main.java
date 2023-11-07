ef7e31bb941fd090301178427a97db75d6d7e7e9
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-11-07 09:34:08 +0700

    add the Main class

---
+++ src/lab_3/question4/Main.java
@@ -0,0 +1,17 @@
+package question4;
+
+//testing
+public class Main {
+    public static void main(String[] args) {
+
+        //creating objects
+        Point2D p2d = new Point2D(14, 30);
+        Point3D p3d = new Point3D(10, 20, 30);
+
+        // printing
+        //2D
+        System.out.println(p2d.toString());
+        //3D
+        System.out.println(p3d.toString());
+    }
+}
