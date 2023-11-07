9aa10519bbb99661e48f87ff391a03a13f4138c9
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-11-07 09:26:21 +0700

    add the MyBoundedShape class

---
+++ src/lab_3/question1/MyBoundedShape.java
@@ -0,0 +1,7 @@
+package question1;
+
+// MyBoundedShape is an abstract class that extends MyShape
+// It has an abstract method getArea()
+public abstract class MyBoundedShape extends MyShape {
+    public abstract float GetArea();
+}
