f17853957576141267f8eee95be36310ea820f15
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-11-09 07:59:49 +0700

    new uppdate

--- src/quizz/EngCourse.java
+++
@@ -1,6 +0,0 @@
-//EngCourse class
-public class EngCourse extends Course {
-    public EngCourse(String name, int minEngScore) {
-        super(name, minEngScore);
-    }
-}
--- src/quizz/PTCourse.java
+++
@@ -1,6 +0,0 @@
-//PTCourse  class
-public class PTCourse extends Course {
-    public PTCourse(String name, int minEngScore) {
-        super(name, minEngScore);
-    }
-}
